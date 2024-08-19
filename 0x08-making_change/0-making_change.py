014d1d5ff7608ff916d0f71b32d056a2cf86c01c
Banji Zillight Olawole <107891531+Zillight@users.noreply.github.com>
2024-08-19 17:36:03 +0100

    chngs

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-"""
-   Fewest number of coins needed from pile of coins
-   to meet a given amount
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
-    """Returns fewest number of coins or -1 if not fulfiled"""
+    """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-    coins.sort()
-    coins.reverse()
-    count = 0
-    summation = 0
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        while summation < total:
-            summation += coin
-            count += 1
-            if summation == total:
-                return count
-            if summation > total:
-                summation -= coin
-                count -= 1
-                break
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
     return -1
